e0e67ee4e612d1ae6b5215e2233309832344411d
fatima ahmed <fatymah001@gmail.com>
2019-08-27 23:42:11 +0300

    Fix stickler errors

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,13 +4,13 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
   test 'should get home' do
     get root_url
     assert_response :success
-    assert_select 'title',  "#{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
 
   test 'should get help' do
