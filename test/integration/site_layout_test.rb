20c90014762019a9c506a18729e22af5ff58278f
fatima ahmed <fatymah001@gmail.com>
2019-09-09 20:57:05 +0300

    Add pagination

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout_links" do
+  test "layout_links for a not logged in user" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -15,9 +15,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title('Contact')
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+  test "layout_links for a logged in user" do
     @user = users(:archer)
     log_in_as(@user)
-    
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -28,9 +29,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
-
     get users_path
     assert_select "title", full_title("All users")
-
   end
 end
