2f92a0f1a6b6d743dae93784890e813f0600e01b
fatima ahmed <fatymah001@gmail.com>
2019-09-10 19:28:30 +0300

    Add mailer templates, email previews and tests

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,7 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :action_activations, onli: [:edit]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
