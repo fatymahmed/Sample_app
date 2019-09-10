6dd81e782d94e917dca9b6232fa5d4ac7613dce5
fatima ahmed <fatymah001@gmail.com>
2019-09-10 22:35:39 +0300

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :action_activations, only: [:edit]
+  resources :account_activations, only: [:edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
