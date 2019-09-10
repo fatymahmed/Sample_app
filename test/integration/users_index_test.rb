848cc592c59caafb204cf6bb767141b15fb467aa
fatima ahmed <fatymah001@gmail.com>
2019-09-10 23:19:58 +0300

    Add more integration tests for account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,6 +6,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
+    @malori = users(:malori)
   end
 
   test 'index as admin including pagination and delete links' do
@@ -15,12 +16,19 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
+      assert user.activated?
       assert_select 'a[href=?]', user_path(user), text: user.name
       assert_select 'a[href=?]', user_path(user), text: 'delete' unless user == @admin
     end
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
+  end
+  test 'Only activated user can log in' do
+    log_in_as(@malori)
+    get user_path(@malori)
+    assert_redirected_to root_url
+    assert_not @malori.activated?
   end
 
   test 'index as non-admin' do
