34d05bfc8f04dc8c6a74d2e34c5583aa9a5fc93e
fatima ahmed <fatymah001@gmail.com>
2019-09-19 01:10:05 +0300

    Add basic image upload and tests for it

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,9 +18,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    assert @user.microposts.paginate(page: 1).first.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -37,7 +40,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost sidebar count" do
     log_in_as(@user)
     get root_path
-    assert_match "#{Micropost.count} microposts", response.body
+    assert_match "#{@user.microposts.count} microposts", response.body
     # User with zero microposts
     other_user= users(:malory)
     log_in_as(other_user)
